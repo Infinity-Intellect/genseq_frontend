9187f04fe85a29b82648797961cd2123b4c16894
Sidd <snavaneetharan@gmail.com>
2020-04-12 02:18:31 +0530

    Updates

---
+++ precache-manifest.b066eb59686f9206fed448025b4ce155.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cc362fa643f39c64e03a671bcda38aa",
+    "url": "/genseq_frontend/index.html"
+  },
+  {
+    "revision": "a2d76fd5e918d214ddf4",
+    "url": "/genseq_frontend/static/css/main.02b43b8e.chunk.css"
+  },
+  {
+    "revision": "0649e7c6f022062ddc19",
+    "url": "/genseq_frontend/static/js/2.cdec84ea.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/genseq_frontend/static/js/2.cdec84ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2d76fd5e918d214ddf4",
+    "url": "/genseq_frontend/static/js/main.88a45db2.chunk.js"
+  },
+  {
+    "revision": "48f49756d6c98964ca76",
+    "url": "/genseq_frontend/static/js/runtime-main.66d5e260.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.53700deb10569597561adb5c0c197086.js"
+  "/genseq_frontend/precache-manifest.b066eb59686f9206fed448025b4ce155.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/genseq_frontend/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
